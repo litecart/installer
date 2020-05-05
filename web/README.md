afc89a4681c9563ddb3d8fbbf1252a3fdf296a38
T. Almroth <info@tim-international.net>
2020-05-06 00:19:58 +0200

    + Add web based installer

---
+++ web/README.md
@@ -0,0 +1,34 @@
+        __    _ __       ______           __
+       / /   (_) /____  / ____/___ ______/ /_
+      / /   / / __/ _ \/ /   / __ `/ ___/ __/
+     / /___/ / /_/  __/ /___/ /_/ / /  / /_
+    /_____/_/\__/\___/\____/\__,_/_/   \__/
+                          www.litecart.net
+
+# Instructions
+
+To install the latest version of LiteCart from a terminal window, do the following:
+
+1. Navigate to the folder reserved for your web content.
+
+    Example: cd "/var/www/mydomain.com/public_html"
+
+2. Download the web installer:
+
+  **Using wget**
+
+    wget -O - https://raw.githubusercontent.com/litecart/installer/master/web/index.php
+
+  **Or curl**
+
+    curl -s https://raw.githubusercontent.com/litecart/installer/master/web/index.php
+
+  **Or manually**
+
+    Grab it from https://raw.githubusercontent.com/litecart/installer/master/web/index.php and store it in the web directory.
+
+3. Open your browser and access your webfolder:
+
+    Example: http://www.mydomain.com/
+
+4. The application will be automatically and the installation wizard will open. Follow the steps on the screen.
---
+++ web/index.php
@@ -0,0 +1,58 @@
+<?php
+
+  ob_start();
+
+  error_reporting(version_compare(PHP_VERSION, '5.4.0', '<') ? E_ALL | E_STRICT : E_ALL);
+
+  ini_set('display_errors', 'On');
+
+  header('Content-Type: text/plain; charset=UTF-8');
+
+  try {
+
+    echo "Downloading the application...\n";
+    if (!$data = file_get_contents('https://www.litecart.net/en/downloading?version=latest&action=get')) {
+      throw new Exception('Failed downloading the application');
+    }
+
+    echo "Reserving a temporary file...\n";
+    if (!$tmpfile = tempnam(sys_get_temp_dir(), 'litecart')) {
+      throw new Exception('Failed creating temporary archive');
+    }
+
+    echo "Creating temporary archive $tmpfile...\n";
+    if (!file_put_contents($tmpfile, $data)) {
+      throw new Exception('Failed storing temporary archive');
+    }
+
+    echo "Opening the archive...\n";
+    $zip = new ZipArchive;
+    if (!$zip->open($tmpfile)) {
+      throw new Exception('Failed opening the archive');
+    }
+
+    echo "Extracting files...\n";
+    for($i = 0; $i < $zip->numFiles; $i++) {
+      $filename = $zip->getNameIndex($i);
+      if (preg_match('#^public_html/(.+)$#', $filename, $matches)) {
+        echo "  $filename\n";
+        if (substr($filename, -1) == '/') {
+          mkdir($matches[1]);
+        } else {
+          copy("zip://$tmpfile#$filename", $matches[1]);
+        }
+      }
+    }
+
+    $zip->close();
+
+    unlink($tmpfile);
+
+    echo "\nExtraction complete! Now open your web browser and navigate to the website to continue the installation.";
+
+    header('Location: install/index.php');
+    exit;
+
+  } catch (Exception $e) {
+    echo 'Error: '. $e->getMessage();
+  }
