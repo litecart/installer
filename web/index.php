d5be77928ffcf42c1e1709e90a12ca09c097d869
T. Almroth <info@tim-international.net>
2022-12-28 16:34:31 +0100

    * Minor adjustments and add some error feedback

--- web/index.php
+++ web/index.php
@@ -5,6 +5,7 @@
   error_reporting(version_compare(PHP_VERSION, '5.4.0', '<') ? E_ALL | E_STRICT : E_ALL);
 
   ini_set('display_errors', 'On');
+  ini_set('display_html_errors', 'Off');
 
   header('Content-Type: text/plain; charset=UTF-8');
 
@@ -34,20 +35,28 @@
     }
 
     echo "Opening the archive...\n";
-    $zip = new ZipArchive;
-    if (!$zip->open($tmpfile)) {
+    $zip = new ZipArchive();
+
+    if ($zip->open($tmpfile, ZipArchive::RDONLY) !== true) {
       throw new Exception('Failed opening the archive');
     }
 
     echo "Extracting files...\n";
     for($i = 0; $i < $zip->numFiles; $i++) {
       $filename = $zip->getNameIndex($i);
+
       if (preg_match('#^public_html/(.+)$#', $filename, $matches)) {
-        echo "  $filename\n";
+        echo "  $matches[1]\n";
+
         if (substr($filename, -1) == '/') {
-          mkdir($matches[1]);
+          if (!is_dir($matches[1]) && !mkdir($matches[1])) {
+            throw new Exception('Failed creating directory '. $matches[1] .' (user: '. get_current_user() .', whoami: '. exec('whoami') .', perms: '. substr(sprintf('%o', fileperms(getcwd())), -4) .')');
+          }
+
         } else {
-          copy("zip://$tmpfile#$filename", $matches[1]);
+          if (!copy("zip://$tmpfile#$filename", $matches[1])) {
+            throw new Exception('Failed writing file '. $matches[1] .' (user: '. get_current_user() .', whoami: '. exec('whoami') .', perms: '. substr(sprintf('%o', fileperms(getcwd())), -4) .')');
+          }
         }
       }
     }
@@ -56,11 +65,17 @@
 
     unlink($tmpfile);
 
-    echo "\nExtraction complete! Now open your web browser and navigate to the website to continue the installation.";
+    echo "\nExtraction complete!\n\n";
 
-    header('Location: install/index.php');
+    if (php_sapi_name() == 'cli') {
+      echo "\nAccess https://yourdomain.tld/install/ from your web browser to begin the installation.";
+      exit;
+    }
+
+    header('Refresh: 5; url=install/');
     exit;
 
   } catch (Exception $e) {
+    if (is_file($tmpfile)) unlink($tmpfile);
     echo 'Error: '. $e->getMessage();
   }
